2170a1d99c71cb7ecfae4303cebe97e12758c38a
Boasfar27 <muhammadfarhan2747@gmail.com>
2026-04-21 21:23:56 +0700

    update herbyy

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function validateForm() {
     var password = document.getElementById("password").value;
 
     //validasi username dan password, username dan password bisa diganti sesuai keinginan
-    if (username === "pacar kalian" && password === "pacar kalian") {
+    if (username === "herbyy" && password === "fikram") {
         Swal.fire({
             icon: "success",
             title: "Login berhasil!",
-            text: "Selamat datang pacar kaliankk",
+            text: "Selamat datang Herby dyah kartini",
             showConfirmButton: false,
             timer: 1500,
         }).then(function () {
